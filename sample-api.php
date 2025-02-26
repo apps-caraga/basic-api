04c0e7ecae1c1998f2f83b5e7c9f4fc53408bb1e
jaleonardo <leonardojohn.a@gmail.com>
2025-02-27 06:20:01 +0800

    Added `is_admin` flag to users table Updated sample API to check for `is_admin` flag

--- sample-api.php
+++ sample-api.php
@@ -8,8 +8,11 @@ use Tqdev\PhpCrudApi\RequestFactory;
 use Tqdev\PhpCrudApi\ResponseUtils;
 
 require_once 'api.include.php';
- 
- 
+
+
+function is_admin(){
+	return (bool) $_SESSION['user']['is_admin'];
+}
 $config = new Config([
         'driver' => 'sqlite',
         'address' => 'database.sqlite',
@@ -19,9 +22,9 @@ $config = new Config([
 		"dbAuth.loginTable"=>"active_users",
 		'authorization.tableHandler' => function ($operation, $tableName) {
             $current_role = $_SESSION['user']['role'];
-
 			$admin_tables = ['roles']; //array of tables accessible to admins only
-		 	if($current_role =='ADMIN'){
+
+		 	if(is_admin()){
 				return true;
 			}else{ // not admin, return false if requested table is for admins only
 				return (!in_array($tableName,$admin_tables));
@@ -33,13 +36,19 @@ $config = new Config([
 		},
 		'multiTenancy.handler' => function ($operation, $tableName) {
             $current_role = $_SESSION['user']['role'];
-            if($current_role ==='ADMIN'){
-                return [];
+            if(is_admin()){	
+                return []; // no filter/show all
             }
             else{ 
-                // this attaches "WHERE created_by= < user id" to every requests
+                // this attaches "WHERE created_by=`id` " to every requests
                 // this can be further customized based on the $operation type as well as the $tableName being accessed
-                return ['created_by' => $_SESSION['user']['id']];
+				if($tableName == 'users' || $tableName == 'active_users'){
+					// return filtered data based on current user ID; users table has no 'created_by' column, so filter by 'id'
+					return ['id' => $_SESSION['user']['id']]; 
+				}else{ 
+					// return filtered data based on current user ID; assumes all other tables have a 'created_by' column
+					// but if it has no 'created_by' column, the filter has no effect
+					return ['created_by' => $_SESSION['user']['id']];				}
             }
 				
 		},
